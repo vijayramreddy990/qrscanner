c8f5840d7b0a9e22e54220d26a66195923dc5e30
vijay <vijayramreddy990@gmail.com>
2021-12-14 17:13:23 +0530

    qr code scanner implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import QRCode from "react-qr-code";
 
 const App = () => {
   return (
     <div>
       <h1>Hii I am going to be a qr scanner</h1>
+      <QRCode value={"123456"} size={290} level="H" />
     </div>
   );
 };
